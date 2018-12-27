155ff38e33f67051706e47a66320564687b981a5
github username <github email address>
2018-12-27 10:46:08 -0500

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,49 @@
-# Write your code here.
+def dictionary
+  {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "For" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet_array = tweet.split(" ")
+  tweet_array.collect do |word|
+    dictionary.collect do |bad_word, good_word|
+      if word == bad_word
+        word.replace(good_word)
+      end
+    end
+  end
+  tweet_array.join(" ")
+end
+
+def bulk_tweet_shortener(array_of_tweets)
+  array_of_tweets.collect do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.chars.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).chars.length > 140
+    word_substituter(tweet).chars[0..136].push("...").join("")
+  else
+    word_substituter(tweet)
+  end
+end
